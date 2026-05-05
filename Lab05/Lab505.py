614f0d4cb7ee9f119eef4b4c71586769388c6d79
GSX <henryw.ramirez@gmail.com>
2026-05-04 21:43:13 -0500

    Laboratorio 5 Completo

---
+++ Lab05/Lab505.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+#thislist.remove ("banana")
+thislist.pop (3)
+print (thislist)
---
+++ Lab05/Lab508.py
@@ -0,0 +1,8 @@
+fruist = ["apple", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruist:
+    if "a" in x:
+        newlist.append (x)
+
+print (newlist)
